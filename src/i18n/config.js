a8dc854f7bbaa8ddb2d539d2efa418fdef904382
justinfarrelldev <justinfarrellwebdev@gmail.com>
2022-08-22 23:44:09 -0400

    ESLint added and ESLint rule violations fixed

--- src/i18n/config.js
+++ src/i18n/config.js
@@ -1,18 +1,18 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-import englishTranslation from './locales/en/translations.json'
+import englishTranslation from './locales/en/translations.json';
 
 i18n.use(initReactI18next).init({
-  fallbackLng: 'en',
-  lng: 'en',
-  resources: {
-    en: {
-      translations: englishTranslation,
+    fallbackLng: 'en',
+    lng: 'en',
+    resources: {
+        en: {
+            translations: englishTranslation,
+        },
     },
-  },
-  ns: ['translations'],
-  defaultNS: 'translations'
+    ns: ['translations'],
+    defaultNS: 'translations',
 });
 
 i18n.languages = ['en'];
