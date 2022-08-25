51268a745995572b6ab77d4866e38e33ad6e197e
justinfarrelldev <justinfarrellwebdev@gmail.com>
2022-08-25 01:09:24 -0400

    Wrote up some i18n initialization for a test to stop it complaining

--- src/pages/Header.test.tsx
+++ src/pages/Header.test.tsx
@@ -4,9 +4,29 @@ import Header from './Header';
 import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import englishTranslation from '../i18n/locales/en/translations.json';
+
 describe('Pages', () => {
     describe('Header', () => {
         describe('Language Selector', () => {
+            beforeAll(() => {
+                i18n.use(initReactI18next).init({
+                    fallbackLng: 'en',
+                    lng: 'en',
+                    resources: {
+                        en: {
+                            translations: englishTranslation,
+                        },
+                    },
+                    ns: ['translations'],
+                    defaultNS: 'translations',
+                });
+
+                i18n.languages = ['en'];
+            });
             it('should be present in the Header', () => {
                 const component = render(
                     <BrowserRouter>
@@ -16,7 +36,6 @@ describe('Pages', () => {
 
                 const element: HTMLElement | null = component.container.querySelector('#lang-select');
                 expect(element).not.toBeNull();
-                if (element) element.click();
             });
         });
     });
