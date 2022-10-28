53df603a0fb919576c49bb7edd6fa95903d6d133
justinfarrelldev <justinfarrellgamedev@gmail.com>
2022-10-28 19:16:03 -0400

    Added plop, plopfile.js (no templates yet)

---
+++ plopfile.js
@@ -0,0 +1,30 @@
+module.exports = function (plop) {
+    plop.setGenerator('create', {
+        description: 'creates a component or page with boilerplate code',
+        prompts: [
+            {
+                type: 'list',
+                name: 'typeToCreate',
+                message: 'Would you like to create a page or a component?',
+                choices: ['page', 'component'],
+            },
+            {
+                type: 'input',
+                name: 'name',
+                message: 'Name: ',
+            },
+            {
+                type: 'input',
+                name: 'description',
+                message: 'Description: ',
+            },
+        ],
+        actions: [
+            {
+                type: 'add',
+                path: 'src/{{ typeToCreate }}s/{{ camelCase name }}/{{ camelCase name }}.tsx',
+                templateFile: 'plop_templates/new-{{ typeToCreate }}.hbs',
+            },
+        ],
+    });
+};
